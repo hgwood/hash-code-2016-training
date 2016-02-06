198c1273387cdc7669de87144bc139dd5d4a5059
Hugo Wood <hx4.5@free.fr>
2016-02-06 19:37:02 +0100

    test: better test for black algo

--- black.test.js
+++ black.test.js
@@ -5,8 +5,16 @@ describe("black algo", function () {
   
   it("", function () {
     const result = black([
-      [false, true, false]
+      [false, true, false],
+      [true, true, false],
+      [false, true, true]
     ])
-    assert.deepEqual(result, [{command: "PAINT_SQUARE", args: [1, 0, 0]}])
+    assert.deepEqual(result, [
+      {command: "PAINT_SQUARE", args: [1, 0, 0]},
+      {command: "PAINT_SQUARE", args: [0, 1, 0]},
+      {command: "PAINT_SQUARE", args: [1, 1, 0]},
+      {command: "PAINT_SQUARE", args: [1, 2, 0]},
+      {command: "PAINT_SQUARE", args: [2, 2, 0]},
+    ])
   })
 })
