ba72e9e94caae3fc9b632eef0db0329b3d59cf4e
Pierre Reliquet <preliquet@restlet.com>
2016-02-06 19:55:15 +0100

    complete test on read

--- io.test.js
+++ io.test.js
@@ -11,6 +11,16 @@ describe('Testing io', function() {
 
   it('should return the structure of the file two squares', function () {
     var array = reader.read('readFile.txt');
+    assert.equal(array[0][0], false);
+    assert.equal(array[0][1], false);
+    assert.equal(array[0][2], false);
+    assert.equal(array[0][3], false);
+    assert.equal(array[0][4], false);
+    assert.equal(array[0][5], true);
+    assert.equal(array[0][6], true);
+    assert.equal(array[0][7], true);
+    assert.equal(array[0][8], true);
+    assert.equal(array[0][9], true);
     assert.equal(array.length, 5);
   });
 });
